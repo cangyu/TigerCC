1d2f7592ec086fbbff99a3199ed204af650b4f55
Yu Cang <yhcy1993@gmail.com>
2017-07-20 16:41:36 +0800

    basic idea

--- src/compiler/Parser/Parser.java
+++ src/compiler/Parser/Parser.java
@@ -11,7 +11,6 @@ public class Parser
 	private Token look;
 	private Env top = null;
 	private int used = 0;
-	
 
 	public Parser(Lexer x) throws IOException
 	{
@@ -19,17 +18,15 @@ public class Parser
 		move();
 	}
 
-	public void parse() throws IOException
-	{
-		
-	}
-
-	private void match(Tag t) throws IOException
+	private boolean match(Tag t) throws IOException
 	{
 		if (look.tag == t)
+		{
 			move();
+			return true;
+		}
 		else
-			panic("Syntax error.");
+			return false;
 	}
 
 	private void move() throws IOException
@@ -45,5 +42,32 @@ public class Parser
 	private void panic(String msg) throws IOException
 	{
 		throw new IOException(String.format("(Line %d, Column %d): %s", look.line, look.column, msg));
+	}
+
+	public void parse() throws IOException
+	{
+		 // program: (declaration | function_definition)+
+		
+		if (look.tag == Tag.EOF)
+			return;
+
+		boolean t1 = declaration();
+		if (!t1)
+		{
+			boolean t2 = function_definition();
+			if (!t2)
+				panic("Unable to match a declaration nor a function_definition.");
+		}
+	}
+
+	private boolean declaration() throws IOException
+	{
+
+		return true;
+	}
+
+	private boolean function_definition() throws IOException
+	{
+		return true;
 	}
 }
