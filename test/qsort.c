a547ad1200f5ab9c01b1c5175856f5fbe7230c1b
Yu Cang <yhcy1993@gmail.com>
2017-08-02 23:20:55 +0800

    alternate to c89 mode

--- test/qsort.c
+++ test/qsort.c
@@ -11,7 +11,9 @@ int partition(int *A, int p, int r)
 {
 	int x = A[r];
 	int i = p - 1;
-	for (int j = p; j <= r - 1; j++)
+	int j = p;
+
+	for (; j <= r - 1; j++)
 	{
 		if (A[j] <= x)
 		{
@@ -27,21 +29,23 @@ int partition(int *A, int p, int r)
 
 void qsort(int *A, int p, int r)
 {
+	int q = -1;
+
 	if (p < r)
 	{
-		int q = partition(A, p, r);
+		q = partition(A, p, r);
 		qsort(A, p, q - 1);
 		qsort(A, q + 1, r);
 	}
 }
 
-
 int main(int argc, char **argv)
 {
-	int a[10] = { 9,8,7,6,5,4,3,2,1,0 };
-	qsort(a, 0, 9);
+	int i = 0;
+	int a[10] = { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
 
-	for (int i = 0; i < 10; i++)
+	qsort(a, 0, 9);
+	for (; i < 10; i++)
 		printf("%d\t", a[i]);
 
 	return 0;
